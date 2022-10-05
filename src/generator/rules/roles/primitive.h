c7f3ee00e6dca4335b6971499517b414e92b6fe8
Paul Höft <hoeftpa@gmail.com>
2022-10-05 11:37:35 +0200

    generate primitive roles for higher arity predicates

--- src/generator/rules/roles/primitive.h
+++ src/generator/rules/roles/primitive.h
@@ -15,13 +15,15 @@ public:
         assert(target_complexity == 1);
         core::SyntacticElementFactory& factory = data.m_factory;
         for (const auto& predicate : factory.get_vocabulary_info_ref().get_predicates_ref()) {
-            if (predicate.get_arity() == 2) {
-                auto element = factory.make_primitive_role(predicate, 0, 1);
-                auto denotations = element.get_element_ref().evaluate(states, caches);
-                if (data.m_role_hash_table.insert(denotations).second) {
-                    data.m_reprs.push_back(element.compute_repr());
-                    data.m_roles_by_iteration[target_complexity].push_back(std::move(element));
-                    increment_generated();
+            for (int arg1 = 0; arg1 < predicate.get_arity(); ++arg1) {
+                for (int arg2 = arg1+1; arg2 < predicate.get_arity(); ++arg2) {
+                    auto element = factory.make_primitive_role(predicate, arg1, arg2);
+                    auto denotations = element.get_element_ref().evaluate(states, caches);
+                    if (data.m_role_hash_table.insert(denotations).second) {
+                        data.m_reprs.push_back(element.compute_repr());
+                        data.m_roles_by_iteration[target_complexity].push_back(std::move(element));
+                        increment_generated();
+                    }
                 }
             }
         }
